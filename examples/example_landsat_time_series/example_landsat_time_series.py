fabade38fe01fcf55324c5c34754222d22aaf096
cankanoa <cankanoa@gmail.com>
2025-05-22 23:46:00 -1000

    Deployed 9d190ad with MkDocs version: 1.6.1

--- examples/example_landsat_time_series/example_landsat_time_series.py
+++ examples/example_landsat_time_series/example_landsat_time_series.py
@@ -1,85 +1,70 @@
 # %% Landsat Time Series
-# This notebook demonstrates how to preprocess Landsat 8-9 into a time series with spectralmatch. Starting from 5 Landsat 8-9 OLI/TIRS C2 L1 images, the process includes clipping clouds with OmniCloudMask, masking high NDVI areas as Pseudo Invariant Features (PIFs), applying global regression Relative Radiometric Normalization, fine-tuning overlap areas with local block adjustment, and before vs after statistics.
-
-# This script is setup to perform matching on all tif files from a folder within the working directory called "Input" e.g. working_directory/Input/*.tif.
-
+# This notebook demonstrates how to preprocess Landsat 8-9 into a time series with spectralmatch.
+# Starting from 5 Landsat 8-9 OLI/TIRS C2 L1 images, the process includes clipping clouds with OmniCloudMask, masking high NDVI areas as Pseudo Invariant Features (PIFs), applying global regression Relative Radiometric Normalization, fine-tuning overlap areas with local block adjustment, and before vs after statistics.
+# This script is set up to perform matching on all tif files from a folder within the working directory called "Input" e.g. working_directory/Input/*.tif.
 
 # %% Setup
 import os
-import importlib
+from spectralmatch import *
 
-from spectralmatch.match.global_regression import global_regression
-from spectralmatch.match.local_block_adjustment import local_block_adjustment
-from spectralmatch.handlers import merge_rasters
-from spectralmatch.mask import create_cloud_mask_with_omnicloudmask, post_process_raster_cloud_mask_to_vector, create_ndvi_mask, post_process_threshold_to_vector, mask_image_with_vector
-
-working_directory = os.path.join(os.getcwd(), "data_landsat")
+# Important: If this does not automatically find the correct CWD, manually copy the path to the /data_worldview3 folder
+working_directory = '/Users/kanoalindiwe/Downloads/Projects/spectralmatch/docs/examples/data_landsat'
 print(working_directory)
 
+input_folder = os.path.join(working_directory, "Input")
+global_folder = os.path.join(working_directory, "GlobalMatch")
+local_folder = os.path.join(working_directory, "LocalMatch")
+mask_cloud_folder = os.path.join(working_directory, "MaskCloud")
+mask_vegetation_folder = os.path.join(working_directory, "MaskVegetation")
+masked_folder = os.path.join(working_directory, "Masked")
+stats_folder = os.path.join(working_directory, "Stats")
+
+window_size = 128
+num_workers = 5
 
 # %% Create cloud masks
-input_folder = os.path.join(working_directory, "Input")
-output_folder = os.path.join(working_directory, 'Masks');
-os.makedirs(output_folder, exist_ok=True)
-input_image_paths_array = [os.path.join(input_folder, f) for f in os.listdir(input_folder) if
-                           f.lower().endswith(".tif")]
+input_image_paths = search_paths(input_folder, "*.tif")
 
-for path in input_image_paths_array:
+for path in input_image_paths:
     create_cloud_mask_with_omnicloudmask(
         path,
         5,
         3,
         8,
-        os.path.join(output_folder, f"{os.path.splitext(os.path.basename(path))[0]}_CloudMask.tif"),
+        os.path.join(mask_cloud_folder, f"{os.path.splitext(os.path.basename(path))[0]}_CloudMask.tif"),
         # down_sample_m=10
     )
 
-input_mask_rasters = [os.path.join(output_folder, f) for f in os.listdir(output_folder) if f.lower().endswith(".tif")]
+input_mask_rasters_paths = search_paths(mask_cloud_folder, "*.tif")
 
-for path in input_mask_rasters:
+for path in input_mask_rasters_paths:
     post_process_raster_cloud_mask_to_vector(
         path,
-        os.path.join(output_folder, f"{os.path.splitext(os.path.basename(path))[0]}_CloudMask.gpkg"),
+        os.path.join(mask_cloud_folder, f"{os.path.splitext(os.path.basename(path))[0]}.gpkg"),
         None,
         {1: 50},
-        {0: 0, 1: 1, 2: 1, 3: 1}
+        {0: None, 1: 1, 2: 1, 3: 1}
     )
 
-
 # %% Use cloud masks
-input_folder = os.path.join(working_directory, "Input")
-mask_folder = os.path.join(working_directory, "Masks")
-output_folder = os.path.join(working_directory, "Masked")
-input_paths = sorted([os.path.join(input_folder, f) for f in os.listdir(input_folder) if f.lower().endswith(".tif")])
-input_mask_vectors = sorted(
-    [os.path.join(mask_folder, f) for f in os.listdir(mask_folder) if f.lower().endswith(".gpkg")])
-output_paths = sorted(
-    [os.path.join(output_folder, os.path.splitext(os.path.basename(path))[0] + "_CloudMasked.tif") for path in
-     input_paths])
+input_image_paths = search_paths(input_folder, "*.tif")
+input_mask_vectors = search_paths(mask_cloud_folder, "*.gpkg", match_to_paths=(input_image_paths, r"(.*)_CloudMask\.gpkg$"))
+output_paths = create_paths(masked_folder, "$_CloudMasked.tif", input_image_paths)
 
-for input_path, vector_path, output_path in zip(input_paths, input_mask_vectors, output_paths):
+for input_path, vector_path, output_path in zip(input_image_paths, input_mask_vectors, output_paths):
     mask_image_with_vector(
         input_path,
         vector_path,
         output_path,
-        {"value": 0},
-        True
+        ("include", "value", 1),
     )
 
+# %% Create vegetation mask for isolated analysis of vegetation
+input_image_paths = search_paths(input_folder, "*.tif")
+raster_mask_paths = create_paths(mask_vegetation_folder, "$_VegetationMask.tif", input_image_paths)
+vector_mask_paths = create_paths(mask_vegetation_folder, "$.gpkg", input_image_paths)
 
-# %% Mask trees as a non-PIF for isolated analysis
-input_folder = os.path.join(working_directory, "Input")
-mask_folder = os.path.join(working_directory, "Pifs")
-output_folder = os.path.join(working_directory, "Pifed")
-input_paths = sorted([os.path.join(input_folder, f) for f in os.listdir(input_folder) if f.lower().endswith(".tif")])
-output_raster_masks = sorted(
-    [os.path.join(mask_folder, os.path.splitext(os.path.basename(path))[0] + "_CloudMasked.tif") for path in
-     input_paths])
-output_vectors_masks = sorted(
-    [os.path.join(mask_folder, os.path.splitext(os.path.basename(path))[0] + "_CloudMasked.gpkg") for path in
-     input_paths])
-
-for input_path, raster_path, vector_path in zip(input_paths, output_raster_masks, output_vectors_masks):
+for input_path, raster_path in zip(input_image_paths, raster_mask_paths):
     create_ndvi_mask(
         input_path,
         raster_path,
@@ -87,55 +72,97 @@ for input_path, raster_path, vector_path in zip(input_paths, output_raster_masks
         4,
     )
 
-for input_path, raster_path, vector_path in zip(input_paths, output_raster_masks, output_vectors_masks):
+for raster_path, vector_path in zip(raster_mask_paths, vector_mask_paths):
     post_process_threshold_to_vector(
         raster_path,
         vector_path,
-        0.2,
-        "<=",
+        0.1,
+        ">=",
     )
 
+# %% Merge vegetation mask to create an inverted-PIF vector
+# This is just a simple example of creating PIFs based on NDVI values, for a more robust methodology use other techniques to create a better mask vector file
+
+input_vector_paths = search_paths(mask_vegetation_folder, "*.gpkg")
+merged_vector_pif_path = os.path.join(working_directory, "Pifs.gpkg")
+
+merge_vectors(
+    input_vector_paths,
+    merged_vector_pif_path,
+    create_name_attribute=("image", ", "),
+    method="intersection",
+    # method="keep_all", # Create a unique mask per image
+    )
 
 # %% Global matching
-vector_mask_path = working_directory + "/Input/Masks.gpkg"
-input_folder = os.path.join(working_directory, "Masked")
-global_folder = os.path.join(working_directory, "GlobalMatch")
-input_image_paths = [os.path.join(input_folder, f) for f in os.listdir(input_folder) if f.lower().endswith(".tif")]
+vector_mask_path = os.path.join(working_directory , "Pifs.gpkg")
 
 global_regression(
-    input_image_paths,
-    global_folder,
+    (masked_folder, "*.tif"),
+    (global_folder, "$_GlobalMatch.tif"),
     custom_mean_factor = 3, # Defualt 1; 3 often works better to 'move' the spectral mean of images closer together
-    custom_std_factor = 1,
-    # vector_mask_path=vector_mask_path,
-    debug_logs=False,
-    window_size=(512, 512),
-    parallel=True,
+    vector_mask_path=("exclude", vector_mask_path),
+    # vector_mask_path=("exclude", vector_mask_path, "image"), # Use unique mask per image
+    window_size=window_size,
+    parallel_workers=num_workers,
+    debug_logs=True,
     )
-
 
 # %% Local matching
-global_folder = os.path.join(working_directory, "GlobalMatch")
-input_image_paths = [os.path.join(global_folder, f) for f in os.listdir(global_folder) if f.lower().endswith(".tif")]
-local_folder = os.path.join(working_directory, "LocalMatch")
-global_image_paths_array = [os.path.join(global_folder, f) for f in os.listdir(global_folder) if f.lower().endswith(".tif")]
+vector_mask_path = os.path.join(working_directory , "Pifs.gpkg")
 
-matched_local_images_paths = local_block_adjustment(
-    global_image_paths_array,
-    local_folder,
+local_block_adjustment(
+    (global_folder, "*.tif"),
+    (local_folder, "$_LocalMatch.tif"),
     number_of_blocks=100,
-    debug_logs=False,
-    window_size=(512, 512),
-    parallel=True,
+    vector_mask_path=("exclude", vector_mask_path),
+    # vector_mask_path=("exclude", vector_mask_path, "image"), # Use unique mask per image
+    window_size=window_size,
+    parallel_workers=num_workers,
+    debug_logs=True,
+    save_block_maps=(os.path.join(local_folder, "ReferenceBlockMap", "ReferenceBlockMap.tif"), os.path.join(local_folder, "LocalBlockMap", "$_LocalBlockMap.tif")),
     )
 
-merge_rasters(
-    matched_local_images_paths, # Rasters are layered with the last ones on top
-    os.path.join(working_directory, "MatchedLocalImages.tif"),
-    window_size=(512, 512),
+# %% Pre-coded quick Statistics
+
+# Compare image spectral profiles
+compare_image_spectral_profiles(
+    input_image_dict={
+        os.path.splitext(os.path.basename(p))[0]: p
+        for p in search_paths(local_folder, "*.tif")
+    },
+    output_figure_path=os.path.join(stats_folder,'LocalMatch_CompareImageSpectralProfiles.png'),
+    title="Global to Local Match Comparison of Image Spectral Profiles",
+    xlabel='Band',
+    ylabel='Reflectance(0-10,000)',
+)
+
+# Compare image spectral profiles pairs
+before_paths = search_paths(input_folder, "*.tif")
+after_paths = search_paths(local_folder, "*.tif")
+
+image_pairs = {
+    os.path.splitext(os.path.basename(b))[0]: [b, a]
+    for b, a in zip(sorted(before_paths), sorted(after_paths))
+    }
+
+compare_image_spectral_profiles_pairs(
+    image_pairs,
+    os.path.join(stats_folder, 'LocalMatch_CompareImageSpectralProfilesPairs.png'),
+    title="Global to Local Match Comparison of Image Spectral Profiles Pairs",
+    xlabel='Band',
+    ylabel='Reflectance(0-10,000)',
     )
 
-print("Done with global and local histogram matching")
+# Compare spatial spectral difference band average
+input_paths = search_paths(input_folder, "*.tif")
+local_paths = search_paths(local_folder, "*.tif")
+before_path, after_path = next(zip(sorted(input_paths), sorted(local_paths)))
 
-
-# %% Statistics
+compare_spatial_spectral_difference_band_average(
+    input_images=[before_path, after_path],
+    output_image_path=os.path.join(stats_folder, 'LocalMatch_CompareSpatialSpectralDifferenceBandAverage.png'),
+    title="Global to Local Match Comparison of Spatial Spectral Difference Band Average",
+    diff_label="Reflectance Difference (0–10,000)",
+    subtitle=f"Image: {os.path.splitext(os.path.basename(before_path))[0]}",
+)
--- examples/example_worldview_mosaic/example_worldview_mosaic.py
+++ examples/example_worldview_mosaic/example_worldview_mosaic.py
@@ -1,77 +1,130 @@
 # %% Worldview Mosaic
 # This file demonstrates how to preprocess Worldview3 imagery into a mosaic using spectralmatch.
 # Starting from two overlapping Worldview3 images in reflectance, the process includes global matching, local matching, starting from saved block maps (optional for demonstration purposes), generating seamlines, and marging images, and before vs after statistics.
-# This script is set up to perform matching on all .tif files from a folder within the working directory called "Input" e.g. working_directory/Input/*.tif.
+# This script is set up to perform matching on all .tif files from a folder within the working directory called "Input" e.g. working_directory/Input/*.tif. The easiest way to process your own imagery is to move it inside that folder or change the working_directory to another folder with this structure, alternatively, you can pass in custom lists of image paths.
 
 # %% Setup
 import os
-import importlib
 
-from spectralmatch.match.global_regression import global_regression
-from spectralmatch.match.local_block_adjustment import local_block_adjustment
-from spectralmatch.handlers import merge_rasters, mask_rasters
-from spectralmatch.voronoi_center_seamline import voronoi_center_seamline
+from fiona.env import local
+from spectralmatch import *
 
-working_directory = '/Users/kanoalindiwe/Downloads/Projects/spectralmatch/docs/examples/data_worldview3' # If this does not automatically find the correct CWD, manually copy the path to the data_worldview3 folder
+# Important: If this does not automatically find the correct CWD, manually copy the path to the /data_worldview3 folder
+working_directory = '/Users/kanoalindiwe/Downloads/Projects/spectralmatch/docs/examples/data_worldview3'
 print(working_directory)
 
 input_folder = os.path.join(working_directory, "Input")
 global_folder = os.path.join(working_directory, "GlobalMatch")
 local_folder = os.path.join(working_directory, "LocalMatch")
-clipped_images = os.path.join(working_directory, "ClippedImages")
+aligned_folder = os.path.join(working_directory, "Aligned")
+clipped_folder = os.path.join(working_directory, "Clipped")
+stats_folder = os.path.join(working_directory, "Stats")
 
 window_size = 128
 num_workers = 5
 
-
 # %% Global matching
 saved_adjustments_path = os.path.join(global_folder, "GlobalAdjustments.json")
 
+
 global_regression(
-    input_folder,
-    (global_folder, "_global"),
-    custom_mean_factor = 3, # Defualt 1; 3 often works better to 'move' the spectral mean of images closer together
+    (input_folder, "*.tif"),
+    (global_folder, "$_Global.tif"),
+    custom_mean_factor = 3, # Default is 1; 3 often works better to 'move' the spectral mean of images closer together
     debug_logs=True,
     window_size=window_size,
     parallel_workers=num_workers,
+    )
+
+# %% (OPTIONAL) Global matching all input images to the spectral profile of any number of specified images (regression will still be based on overlapping areas, however, only the *included* images statistics will influence the solution)
+new_global_folder = os.path.join(working_directory, "GlobalMatch_New")
+saved_adjustments_path = os.path.join(new_global_folder, "GlobalAdjustments.json")
+
+
+global_regression(
+    (input_folder, "*.tif"),
+    (new_global_folder, "$_Global.tif"),
+    custom_mean_factor = 3, # Default is 1; 3 often works better to 'move' the spectral mean of images closer together
+    debug_logs=True,
+    window_size=window_size,
+    parallel_workers=num_workers,
+    specify_model_images=("include", ['worldview3_example_image_right']),
     save_adjustments=saved_adjustments_path,
     )
 
-# %% Local matching
-input_image_paths = [os.path.join(global_folder, f) for f in os.listdir(global_folder) if f.lower().endswith(".tif")]
+# %% (OPTIONAL) Global matching starting from precomputed statistics for images whole and overlap stats
+new_global_folder = os.path.join(working_directory, "GlobalMatch_New")
+saved_adjustments_path = os.path.join(new_global_folder, "GlobalAdjustments.json")
 
+
+global_regression(
+    (input_folder, "*.tif"),
+    (new_global_folder, "$_Global.tif"),
+    custom_mean_factor = 3, # Default is 1; 3 often works better to 'move' the spectral mean of images closer together
+    debug_logs=True,
+    window_size=window_size,
+    parallel_workers=num_workers,
+    load_adjustments=saved_adjustments_path,
+    )
+
+# %% Local matching
 local_block_adjustment(
-    input_image_paths,
-    local_folder,
+    (global_folder, "*.tif"),
+    (local_folder, "$_Local.tif"),
     number_of_blocks=100,
     debug_logs=True,
     window_size=window_size,
     parallel_workers=num_workers,
-    save_block_maps=True,
     )
 
-# %% Start from saved block maps (optional)
-input_image_paths = [os.path.join(global_folder, f) for f in os.listdir(global_folder) if f.lower().endswith(".tif")]
+# %% (OPTIONAL) Local match with a larger canvas than images bounds (perhaps to anticipate adding additional imagery so you don't have to recalculate local block maps each rematch)
+new_local_folder = os.path.join(working_directory, "LocalMatch_New")
+reference_map_path = os.path.join(new_local_folder, "ReferenceBlockMap", "ReferenceBlockMap.tif")
+local_maps_path = os.path.join(new_local_folder, "LocalBlockMap", "$_LocalBlockMap.tif")
+
+local_block_adjustment(
+    (global_folder, "*.tif"),
+    (new_local_folder, "$_Local.tif"),
+    number_of_blocks=(30,30),
+    debug_logs=True,
+    window_size=window_size,
+    parallel_workers=num_workers,
+    override_bounds_canvas_coords = (193011.1444011169369332, 2184419.3597142999060452, 205679.2836037494416814, 2198309.8632259583100677),
+    save_block_maps=(reference_map_path, local_maps_path),
+    )
+
+# %% (OPTIONAL) Local match from saved block maps (this code just passes in local maps, but if a reference map is passed in, it will match images to the reference map without recomputing it)
 
 old_local_folder = os.path.join(working_directory, "LocalMatch")
 new_local_folder = os.path.join(working_directory, "LocalMatch_New")
-
-saved_reference_block_path = os.path.join(old_local_folder, "BlockReferenceMean", "BlockReferenceMean.tif")
-saved_local_block_paths = [os.path.join(os.path.join(old_local_folder, "BlockLocalMean"), f) for f in os.listdir(os.path.join(old_local_folder, "BlockLocalMean")) if f.lower().endswith(".tif")]
+saved_reference_block_path = os.path.join(old_local_folder, "ReferenceBlockMap", "ReferenceBlockMap.tif")
+saved_local_block_paths = [os.path.join(os.path.join(new_local_folder, "LocalBlockMap"), f) for f in os.listdir(os.path.join(new_local_folder, "LocalBlockMap")) if f.lower().endswith(".tif")]
 
 local_block_adjustment(
-    input_image_paths,
-    new_local_folder,
+    (global_folder, "*.tif"),
+    (new_local_folder, "$_Local.tif"),
     number_of_blocks=100,
     debug_logs=True,
     window_size=window_size,
     parallel_workers=num_workers,
-    load_block_maps=(saved_reference_block_path, saved_local_block_paths)
+    load_block_maps=(None, saved_local_block_paths)
     )
 
+#%% Align rasters
+input_image_paths = search_paths(local_folder, "*.tif")
+output_clipped_image_paths = create_paths(aligned_folder, "$_Aligned.tif", input_image_paths)
+
+process_rasters(
+    input_image_paths,
+    output_clipped_image_paths,
+    tap=True,
+    resolution='highest',
+    debug_logs=True,
+    window_size=window_size,
+    )
 
 # %% Generate seamlines
-input_image_paths = [os.path.join(local_folder, f) for f in os.listdir(local_folder) if f.lower().endswith(".tif")]
+input_image_paths = search_paths(aligned_folder, "*.tif")
 output_vector_mask = os.path.join(working_directory, "ImageClips.gpkg")
 
 voronoi_center_seamline(
@@ -81,19 +134,16 @@ voronoi_center_seamline(
     debug_logs=True,
     )
 
-
 # %% Clip and merge
-input_image_paths = sorted([os.path.join(local_folder, f) for f in os.listdir(local_folder) if f.lower().endswith(".tif")])
-output_clipped_image_paths = sorted([os.path.join(clipped_images, os.path.splitext(os.path.basename(path))[0] + "_Clipped.tif") for path in input_image_paths])
-
+input_image_paths = search_paths(aligned_folder, "*.tif")
+output_clipped_image_paths = create_paths(clipped_folder, "$_Clipped.tif", input_image_paths)
 input_vector_mask_path = os.path.join(working_directory, "ImageClips.gpkg")
 output_merged_image_path = os.path.join(working_directory, "MergedImage.tif")
 
-mask_rasters(
+process_rasters(
     input_image_paths,
     output_clipped_image_paths,
-    input_vector_mask_path,
-    tap=True,
+    vector_mask_path=input_vector_mask_path,
     debug_logs=True,
     split_mask_by_attribute="image",
     window_size=window_size,
@@ -106,53 +156,46 @@ merge_rasters(
     debug_logs=True,
 )
 
-# %% Statistics
-from spectralmatch import (
-    compare_spatial_spectral_difference_individual_bands,
-    compare_image_spectral_profiles_pairs,
-    compare_image_spectral_profiles,
-    compare_spatial_spectral_difference_average)
+# %% Pre-coded quick Statistics
 
-compare_spatial_spectral_difference_individual_bands(
-    (
-    '/image/a.tif',
-    '/image/b.tif'),
-    '/output.png'
+# Compare image spectral profiles
+compare_image_spectral_profiles(
+    input_image_dict={
+        os.path.splitext(os.path.basename(p))[0]: p
+        for p in search_paths(local_folder, "*.tif")
+    },
+    output_figure_path=os.path.join(stats_folder,'LocalMatch_CompareImageSpectralProfiles.png'),
+    title="Global to Local Match Comparison of Image Spectral Profiles",
+    xlabel='Band',
+    ylabel='Reflectance(0-10,000)',
 )
 
+# Compare image spectral profiles pairs
+before_paths = search_paths(input_folder, "*.tif")
+after_paths = search_paths(local_folder, "*.tif")
+
+image_pairs = {
+    os.path.splitext(os.path.basename(b))[0]: [b, a]
+    for b, a in zip(sorted(before_paths), sorted(after_paths))
+    }
 
 compare_image_spectral_profiles_pairs(
-    {
-        'Image A': [
-            '/image/before/a.tif',
-            'image/after/a.tif'
-        ],
-        'Image B': [
-            '/image/before/b.tif',
-            '/image/after/b.tif'
-        ]
-    },
-    '/output.png'
-)
+    image_pairs,
+    os.path.join(stats_folder, 'LocalMatch_CompareImageSpectralProfilesPairs.png'),
+    title="Global to Local Match Comparison of Image Spectral Profiles Pairs",
+    xlabel='Band',
+    ylabel='Reflectance(0-10,000)',
+    )
 
+# Compare spatial spectral difference band average
+input_paths = search_paths(input_folder, "*.tif")
+local_paths = search_paths(local_folder, "*.tif")
+before_path, after_path = next(zip(sorted(input_paths), sorted(local_paths)))
 
-compare_image_spectral_profiles(
-    {
-        'Image A': 'image/a.tif',
-        'Image B': '/image/b.tif'
-    },
-    "/output.png",
-    "Digital Number Spectral Profile Comparison",
-    'Band',
-    'Digital Number(0-2,047)',
-
-)
-
-
-compare_spatial_spectral_difference_average(
-    [
-        '/image/a.tif',
-        '/image/a.tif'
-     ],
-    '/output.png'
+compare_spatial_spectral_difference_band_average(
+    input_images=[before_path, after_path],
+    output_image_path=os.path.join(stats_folder, 'LocalMatch_CompareSpatialSpectralDifferenceBandAverage.png'),
+    title="Global to Local Match Comparison of Spatial Spectral Difference Band Average",
+    diff_label="Reflectance Difference (0–10,000)",
+    subtitle=f"Image: {os.path.splitext(os.path.basename(before_path))[0]}",
 )
